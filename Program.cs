3a18aa4cd560dff0a100d887f5183f1f0fc422f1
gaikwad121 <jg.jyotigaikwad121@gmail.com>
2025-04-14 03:08:32 +0530

    hello

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace project;
+public class Programs
+{
+    static void Main(String[] args)
+    {
+        SwapNoWitThird();
+        SwapNoWithoutThird();
+        SwapNoWithoutThird1();
+    }
+    static void SwapNoWitThird()
+    {
+        int a = 10;
+        int b = 45;
+        Console.WriteLine("Before Swaping" + " " + a + " " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+        Console.WriteLine("After Swaping" + " " + a + " " + b);
+    }
+    static void SwapNoWithoutThird()
+    {
+        int a = 41;
+        int b = 67;
+        Console.WriteLine("Before Swaping" + " " + a + " " + b);
+
+        a = a * b;
+        b = a / b;
+        a = a / b;
+        Console.WriteLine("After Swaping" + " " + a + " " + b);
+
+    }
+    static void SwapNoWithoutThird1()
+    {
+        int a = 41;
+        int b = 67;
+        Console.WriteLine("Before Swaping" + " " + a + " " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        Console.WriteLine("After Swaping" + " " + a + " " + b);
+
+    }
+
+}
