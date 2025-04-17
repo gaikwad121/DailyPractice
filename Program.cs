5e74e164f81d3ae656a2b22281f58e18cf6946a1
Jyoti Gaikwad <98073563+gaikwad121@users.noreply.github.com>
2025-04-18 02:00:13 +0530

    Update Program.cs
    Added a new codes

--- Program.cs
+++ Program.cs
@@ -45,3 +45,55 @@ public class Programs
     }
 
 }
+using System;
+namespace project;
+class Practice_Program
+{
+    static void Main()
+    {
+        // Program1();
+        Program2();
+        Program3();
+        Program4();
+
+    }
+    static void Program1()
+    {
+        int[] arr = new int[5];
+        Console.WriteLine("Enter the values of the array");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(arr[i] + " ");
+        }
+    }
+    static void Program2()
+    {
+        string[] arr = { "apple", "banana", "cherry", "date", "elephant" };
+        Console.WriteLine(string.Join(", ", arr)); // Fixed the issue by using string.Join to format the array as a string  
+        Console.WriteLine(string.Format(", ", arr));
+    }
+    static void Program3()
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        Console.WriteLine(arr.Length);
+    }
+    static void Program4()
+    // reverse array
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int i = 0; int j = arr.Length - 1;
+        while (i < j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        foreach (int a in arr)
+        {
+            Console.WriteLine(a);
+        }
+    }
+}
